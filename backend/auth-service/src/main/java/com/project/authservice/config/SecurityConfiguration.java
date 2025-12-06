6e6f460c86f00ead3a67352d62610165f43f002c
unknown <apor.ylmaz@gmail.com>
2025-12-06 18:59:31 +0300

    Update SecurityConfiguration.java

--- backend/auth-service/src/main/java/com/project/authservice/config/SecurityConfiguration.java
+++ backend/auth-service/src/main/java/com/project/authservice/config/SecurityConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +28,26 @@ public class SecurityConfiguration {
                 .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService  userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("aA"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("uU"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Şifrelerin "1234" olarak düz metin saklanmaması için bu gereklidir.
+        return new BCryptPasswordEncoder();
     }
 }
