b365effa20561ed171ea1eadc47876aba916630f
unknown <apor.ylmaz@gmail.com>
2025-12-06 18:39:05 +0300

    auth güncelleme

---
+++ backend/auth-service/src/main/java/com/project/authservice/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.project.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/dashboard").authenticated()
+                        .requestMatchers("/index", "/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+
+        return http.build();
+    }
+}
--- backend/auth-service/src/main/java/com/project/authservice/controller/UserController.java
+++ backend/auth-service/src/main/java/com/project/authservice/controller/UserController.java
@@ -1,4 +1,5 @@
-package controller;
+package com.project.authservice.controller;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,11 @@ public class UserController {
 
     @GetMapping("/index")
     public String Index(){
-        return "index";
+        return "Hello World";
+    }
+
+    @GetMapping("/dashboard")
+    public String Dashboard(){
+        return "Dashboard";
     }
 }
